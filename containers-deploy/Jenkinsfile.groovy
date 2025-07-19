2be4668dc96137eef4067b9c5d6c204ba29bcc49
Lifailon <deadepicboss@gmail.com>
2025-07-19 11:46:59 +0300

    add new dozzle, beszel, watchtower agents and update params

--- containers-deploy/Jenkinsfile.groovy
+++ containers-deploy/Jenkinsfile.groovy
@@ -41,35 +41,51 @@ pipeline {
                             'node_exporter',
                             'cadvisor',
                             'logporter',
-                            'loki_promtail'
+                            'loki_promtail',
+                            'dozzle',
+                            'beszel',
+                            'watchtower'
                         ]
                     '''
                 ]
             ]
         )
+        // string(
+        //     name: 'node_exporter_port',
+        //     defaultValue: '9100'
+        // )
+        // string(
+        //     name: 'cadvisor_port',
+        //     defaultValue: '8080'
+        // )
+        // string(
+        //     name: 'logporter_port',
+        //     defaultValue: '9333'
+        // )
+        // string(
+        //     name: 'loki_promtail_port',
+        //     defaultValue: '9080'
+        // )
         string(
-            name: 'node_exporter_port',
-            defaultValue: '9100'
+            name: 'loki_server',
+            defaultValue: 'http://192.168.3.105:3100'
         )
-        string(
-            name: 'cadvisor_port',
-            defaultValue: '8080'
+        // string(
+        //     name: 'dozzle_port',
+        //     defaultValue: '7007'
+        // )
+        // string(
+        //     name: 'beszel_port',
+        //     defaultValue: '45876'
+        // )
+        password(
+            name: 'beszel_key'
         )
-        string(
-            name: 'logporter_port',
-            defaultValue: '9333'
+        password(
+            name: 'telegram_bot_api_key'
         )
-        string(
-            name: 'loki_promtail_port',
-            defaultValue: '9080'
-        )
-        string(
-            name: 'loki_server_addr',
-            defaultValue: 'http://192.168.3.105'
-        )
-        string(
-            name: 'loki_server_port',
-            defaultValue: '3100'
+        password(
+            name: 'telegram_chat_id'
         )
     }
     stages {
@@ -91,13 +107,16 @@ pipeline {
                 script {
                     echo "Selected containers: ${params.containers}"
                     def containersArr = params.containers.split(',')
-                    def containersArrSelected = [
+                    def containersParams = [
                         node_exporter: containersArr.contains('node_exporter'),
                         cadvisor: containersArr.contains('cadvisor'),
                         logporter: containersArr.contains('logporter'),
-                        loki_promtail: containersArr.contains('loki_promtail')
+                        loki_promtail: containersArr.contains('loki_promtail'),
+                        dozzle: containersArr.contains('dozzle'),
+                        beszel: containersArr.contains('beszel'),
+                        watchtower: containersArr.contains('watchtower')
                     ]
-                    echo "Selected containers: $containersArrSelected"
+                    echo "Selected containers: $containersParams"
                     ansiblePlaybook(
                         // playbook: "/home/jenkins/workspace/ansible/playbooks/deploy.yml",
                         // inventory: "/home/jenkins/workspace/ansible/inventories/inventory.ini",
@@ -106,16 +125,23 @@ pipeline {
                         limit: params.ansible_limit,
                         extraVars: [
                             ansible_port: params.ansible_port,
-                            node_exporter: containersArrSelected.node_exporter,
-                            node_exporter_port: params.node_exporter_port,
-                            cadvisor: containersArrSelected.cadvisor,
-                            cadvisor_port: params.cadvisor_port,
-                            logporter: containersArrSelected.logporter,
-                            logporter_port: params.logporter_port,
-                            loki_promtail: containersArrSelected.loki_promtail,
-                            loki_promtail_port: params.loki_promtail_port,
-                            loki_server_addr: params.loki_server_addr,
-                            loki_server_port: params.loki_server_port
+                            node_exporter: containersParams.node_exporter,
+                            // node_exporter_port: "9100",
+                            cadvisor: containersParams.cadvisor,
+                            // cadvisor_port: "8080",
+                            logporter: containersParams.logporter,
+                            // logporter_port: "9333",
+                            loki_promtail: containersParams.loki_promtail,
+                            // loki_promtail_port: "9080",
+                            loki_server: params.loki_server,
+                            dozzle: containersParams.dozzle,
+                            // dozzle_port: "7007",
+                            beszel: containersParams.beszel,
+                            // beszel_port: "45876",
+                            beszel_key: params.beszel_key,
+                            watchtower: containersParams.watchtower,
+                            telegram_bot_api_key: params.telegram_bot_api_key,
+                            telegram_chat_id: params.telegram_chat_id
                         ],
                         credentialsId: credentials,
                         colorized: true
--- containers-deploy/README.md
+++ containers-deploy/README.md
@@ -1,24 +1,19 @@
 # Containers Deploy
 
-Pipeline implements the deployment of agents for monitoring metrics and logs from Linux and the Docker containers using Ansible. Templating and parameterization are supported for 4 agents: `Node Exporter`, `cAdvisor`, `LogPorter` and `Loki promtail` (if you want to skip installation of a specific agent, leave the port parameter blank).
+Pipeline implements deployment of agents for monitoring metrics and logs from Linux systems and Docker containers using Ansible. Templating and parameterization are supported for the following agents:
 
-## Local run
-
-```bash
-git clone https://github.com/Lifailon/parallel-execution-pipeline
-cd parallel-execution-pipeline/mon-agent-deploy
-
-# Update host list and variables
-# ./inventories/inventory.yml
-# ./inventories/group_vars/*.yml
-
-ansible-playbook -l <HOSTS_GROUP_NAME> -i ./inventories/inventory.yml ./playbooks/deploy.yml
-```
+- [Node Exporter](https://github.com/prometheus/node_exporter)
+- [cAdvisor](https://github.com/google/cadvisor)
+- [LogPorter](https://github.com/Lifailon/logporter)
+- [Loki promtail](https://github.com/grafana/loki)
+- [Dozzle](https://github.com/amir20/dozzle)
+- [Beszel](https://github.com/henrygd/beszel)
+- [Watchtower](https://github.com/containrrr/watchtower)
 
 ## Parameters
 
-![](/mon-agent-deploy/img/params.jpg)
+![](/containers-deploy/img/params.jpg)
 
 ## Deployment
 
-![](/mon-agent-deploy/img/playbook-run.jpg)
+![](/containers-deploy/img/playbook-run.jpg)
